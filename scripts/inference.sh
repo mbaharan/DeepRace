6e8859516a26683c4d0b16c8e0aa5b109e7f5cc6
theDweeb <sfurgurs@uncc.edu>
2020-08-13 15:54:23 -0400

    scripts

---
+++ scripts/inference.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+python3 inference.py --config ./output/train/08_13_20__1547_TCN_21_104_15_3_Dev_0/args.yaml --rul-time 77 83 90 95 101 107 114 117 119 122 123
--- scripts/train1.sh
+++ scripts/train1.sh
@@ -1,2 +1,81 @@
 #!/bin/bash
-python3 train.py --epochs 600 --nhid 15 --ksize 7 --levels 3 --testset 0 --dropout 0
+python3 train.py --nhid 10 --ksize 7 --levels 3 --testset 0 --dropout 0.1  --batch-size 1
+sh /home/steve/Research_Projects/DeepRace/scripts/inference.sh
+python3 train.py --nhid 10 --ksize 7 --levels 3 --testset 1 --dropout 0.1  --batch-size 1
+sh /home/steve/Research_Projects/DeepRace/scripts/inference.sh
+python3 train.py --nhid 10 --ksize 7 --levels 3 --testset 4 --dropout 0.1  --batch-size 1
+sh /home/steve/Research_Projects/DeepRace/scripts/inference.sh
+python3 train.py --nhid 10 --ksize 7 --levels 3 --testset 9 --dropout 0.1  --batch-size 1
+sh /home/steve/Research_Projects/DeepRace/scripts/inference.sh
+
+python3 train.py --nhid 15 --ksize 7 --levels 3 --testset 0 --dropout 0.1  --batch-size 1
+sh /home/steve/Research_Projects/DeepRace/scripts/inference.sh
+python3 train.py --nhid 15 --ksize 7 --levels 3 --testset 1 --dropout 0.1  --batch-size 1
+sh /home/steve/Research_Projects/DeepRace/scripts/inference.sh
+python3 train.py --nhid 15 --ksize 7 --levels 3 --testset 4 --dropout 0.1  --batch-size 1
+sh /home/steve/Research_Projects/DeepRace/scripts/inference.sh
+python3 train.py --nhid 15 --ksize 7 --levels 3 --testset 9 --dropout 0.1  --batch-size 1
+sh /home/steve/Research_Projects/DeepRace/scripts/inference.sh
+
+python3 train.py --nhid 20 --ksize 7 --levels 3 --testset 0 --dropout 0.1  --batch-size 1
+sh /home/steve/Research_Projects/DeepRace/scripts/inference.sh
+python3 train.py --nhid 20 --ksize 7 --levels 3 --testset 1 --dropout 0.1  --batch-size 1
+sh /home/steve/Research_Projects/DeepRace/scripts/inference.sh
+python3 train.py --nhid 20 --ksize 7 --levels 3 --testset 4 --dropout 0.1  --batch-size 1
+sh /home/steve/Research_Projects/DeepRace/scripts/inference.sh
+python3 train.py --nhid 20 --ksize 7 --levels 3 --testset 9 --dropout 0.1  --batch-size 1
+sh /home/steve/Research_Projects/DeepRace/scripts/inference.sh
+
+python3 train.py --nhid 10 --ksize 7 --levels 3 --testset 0 --dropout 0.2  --batch-size 1
+sh /home/steve/Research_Projects/DeepRace/scripts/inference.sh
+python3 train.py --nhid 10 --ksize 7 --levels 3 --testset 1 --dropout 0.2  --batch-size 1
+sh /home/steve/Research_Projects/DeepRace/scripts/inference.sh
+python3 train.py --nhid 10 --ksize 7 --levels 3 --testset 4 --dropout 0.2  --batch-size 1
+sh /home/steve/Research_Projects/DeepRace/scripts/inference.sh
+python3 train.py --nhid 10 --ksize 7 --levels 3 --testset 9 --dropout 0.2  --batch-size 1
+sh /home/steve/Research_Projects/DeepRace/scripts/inference.sh
+
+python3 train.py --nhid 15 --ksize 7 --levels 3 --testset 0 --dropout 0.2  --batch-size 1
+sh /home/steve/Research_Projects/DeepRace/scripts/inference.sh
+python3 train.py --nhid 15 --ksize 7 --levels 3 --testset 1 --dropout 0.2  --batch-size 1
+sh /home/steve/Research_Projects/DeepRace/scripts/inference.sh
+python3 train.py --nhid 15 --ksize 7 --levels 3 --testset 4 --dropout 0.2  --batch-size 1
+sh /home/steve/Research_Projects/DeepRace/scripts/inference.sh
+python3 train.py --nhid 15 --ksize 7 --levels 3 --testset 9 --dropout 0.2  --batch-size 1
+sh /home/steve/Research_Projects/DeepRace/scripts/inference.sh
+
+python3 train.py --nhid 20 --ksize 7 --levels 3 --testset 0 --dropout 0.2  --batch-size 1
+sh /home/steve/Research_Projects/DeepRace/scripts/inference.sh
+python3 train.py --nhid 20 --ksize 7 --levels 3 --testset 1 --dropout 0.2  --batch-size 1
+sh /home/steve/Research_Projects/DeepRace/scripts/inference.sh
+python3 train.py --nhid 20 --ksize 7 --levels 3 --testset 4 --dropout 0.2  --batch-size 1
+sh /home/steve/Research_Projects/DeepRace/scripts/inference.sh
+python3 train.py --nhid 20 --ksize 7 --levels 3 --testset 9 --dropout 0.2  --batch-size 1
+sh /home/steve/Research_Projects/DeepRace/scripts/inference.sh
+
+python3 train.py --nhid 10 --ksize 7 --levels 3 --testset 0 --dropout 0.3  --batch-size 1
+sh /home/steve/Research_Projects/DeepRace/scripts/inference.sh
+python3 train.py --nhid 10 --ksize 7 --levels 3 --testset 1 --dropout 0.3  --batch-size 1
+sh /home/steve/Research_Projects/DeepRace/scripts/inference.sh
+python3 train.py --nhid 10 --ksize 7 --levels 3 --testset 4 --dropout 0.3  --batch-size 1
+sh /home/steve/Research_Projects/DeepRace/scripts/inference.sh
+python3 train.py --nhid 10 --ksize 7 --levels 3 --testset 9 --dropout 0.3  --batch-size 1
+sh /home/steve/Research_Projects/DeepRace/scripts/inference.sh
+
+python3 train.py --nhid 15 --ksize 7 --levels 3 --testset 0 --dropout 0.3  --batch-size 1
+sh /home/steve/Research_Projects/DeepRace/scripts/inference.sh
+python3 train.py --nhid 15 --ksize 7 --levels 3 --testset 1 --dropout 0.3  --batch-size 1
+sh /home/steve/Research_Projects/DeepRace/scripts/inference.sh
+python3 train.py --nhid 15 --ksize 7 --levels 3 --testset 4 --dropout 0.3  --batch-size 1
+sh /home/steve/Research_Projects/DeepRace/scripts/inference.sh
+python3 train.py --nhid 15 --ksize 7 --levels 3 --testset 9 --dropout 0.3  --batch-size 1
+sh /home/steve/Research_Projects/DeepRace/scripts/inference.sh
+
+python3 train.py --nhid 20 --ksize 7 --levels 3 --testset 0 --dropout 0.3  --batch-size 1
+sh /home/steve/Research_Projects/DeepRace/scripts/inference.sh
+python3 train.py --nhid 20 --ksize 7 --levels 3 --testset 1 --dropout 0.3  --batch-size 1
+sh /home/steve/Research_Projects/DeepRace/scripts/inference.sh
+python3 train.py --nhid 20 --ksize 7 --levels 3 --testset 4 --dropout 0.3  --batch-size 1
+sh /home/steve/Research_Projects/DeepRace/scripts/inference.sh
+python3 train.py --nhid 20 --ksize 7 --levels 3 --testset 9 --dropout 0.3  --batch-size 1
+sh /home/steve/Research_Projects/DeepRace/scripts/inference.sh
